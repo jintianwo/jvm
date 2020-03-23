3da11250effa318230b6c233514a7da4d5a667f6
libusheng <libusheng@dangdang.com>
2020-03-23 10:54:52 +0800

    init

---
+++ src/main/java/com/shengsiyuan/jvm/classloader/MyTest1.java
@@ -0,0 +1,28 @@
+package com.shengsiyuan.jvm.classloader;
+
+/**
+ * 主动使用被动使用的实例讲解
+ * 1。对于静态字段来说，只有直接定义了该字段的类才会被初始化
+ * 2。当一个类在初始化时，要求其全部父类都已经初始化完成了
+ */
+public class MyTest1 {
+    public static void main(String[] args) {
+        System.out.println(MyChild1.str);
+    }
+}
+
+class MyParent1{
+    public static String str = "ddd";
+
+    static {
+        System.out.println("myparent1");
+    }
+}
+
+class MyChild1 extends MyParent1{
+    public static String str2 = "ddd2222";
+
+    static {
+        System.out.println("mychild1");
+    }
+}
